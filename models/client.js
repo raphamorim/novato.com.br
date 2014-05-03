33784d938624f870e6df19401e33234670d40655
Raphael Amorim <rapha850@gmail.com>
2014-05-03 04:47:30 -0300

    add client model to project

---
+++ models/client.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt');
+
+var client = new Schema({
+    fbId: { type: Number, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    name: String,
+    city: String,
+    type: String,
+
+    newbie: {
+        website: String,
+        about: String,
+        academic: String,
+        college: String,
+        design: Number,
+        development: Number,
+        illustration: Number
+    },
+
+    enterprise: {
+        name: String,
+        website: String,
+        size: Number,
+        categories: String
+    }
+
+}, {
+    versionKey: false
+});
+
+client.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+module.exports = mongoose.model('Client', client);
